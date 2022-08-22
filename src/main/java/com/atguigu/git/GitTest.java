bb3fbdab8bca24ce41dd24e74d018ca6a74f1a42
zhaozhao <zhaozhao@qq.com>
2022-08-22 23:58:51 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author zhangzhao
+ * @Date 2022/8/22
+ * @description
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
