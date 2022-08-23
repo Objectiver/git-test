30d09c47ddbf2b07c2ef9d965f1813a2b663d73a
zhaozhao <zhaozhao@qq.com>
2022-08-23 20:01:35 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -6,10 +6,13 @@ package com.atguigu.git;
  * @description
  */
 public class GitTest {
+
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("hello git2!");
         System.out.println("hello git3!");
         System.out.println("hello git4!");
+        System.out.println("hot-fix test!");
     }
+
 }
